af606b7d137cf05361f033bff6791e2492e39aba
John Reitano <john.reitano@wearesalty.com>
2021-03-19 17:37:32 -0700

    update formula

--- simplestack.rb
+++ simplestack.rb
@@ -2,8 +2,8 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Simplestack < Formula
-  desc ""
-  homepage ""
+  desc "A command-line tool to generate an application following the SimpleStack pattern"
+  homepage "https://github.com/johnreitano/simplestack"
   url "https://github.com/johnreitano/simplestack/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "ebbe37762a186749b4143148b0068f45f15895a8543604c440b782bbed04db25"
   license ""
@@ -19,16 +19,16 @@ class Simplestack < Formula
     bin.install "simplestack"
   end
 
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test simplestack`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
-  end
+  # test do
+  #   # `test do` will create, run in and delete a temporary directory.
+  #   #
+  #   # This test will fail and we won't accept that! For Homebrew/homebrew-core
+  #   # this will need to be a test that verifies the functionality of the
+  #   # software. Run the test with `brew test simplestack`. Options passed
+  #   # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+  #   #
+  #   # The installed folder is not in the path, so use the entire path to any
+  #   # executables being tested: `system "#{bin}/program", "do", "something"`.
+  #   system "false"
+  # end
 end
